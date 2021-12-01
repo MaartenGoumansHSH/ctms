e299cebb7fd3a1c861e7a4f657e9c186e6dc3b90
MaartenGoumansHSH <mg@hsh.nl>
2021-12-01 13:59:04 +0100

    2 files modified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
-
-
-    var sequence = new CanvasSequence('canvas', 'img/sequence_00', 98, 180, '.jpg', function() {
-        console.log("Loaded !")
-    });
-
+$(document).ready(function () {
+  var sequence = new CanvasSequence(
+    'canvas',
+    'https://github.com/MaartenGoumansHSH/ctms/blob/master/img/sequence_00',
+    98,
+    180,
+    '.jpg',
+    function () {
+      console.log('Loaded !');
+    }
+  );
 });
